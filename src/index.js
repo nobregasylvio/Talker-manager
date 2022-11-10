5c87ce351894e0e521645fc1579c61cc29831867
Sylvio Nobrega Soares <sylvionobregasoares@gmail.com>
2022-11-10 15:52:53 -0300

    Requisito 02

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const talker = require('./models/talker');
+const talker = require('./models/talker.models');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
+const HTTP_ERROR_STATUS = 404;
 const PORT = '3000';
 
 // Não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
+app.get('/', (_req, res) => {
+  res.status(HTTP_OK_STATUS).send();
 });
 
-app.get('/talker', async (_request, response) => {
+app.get('/talker', async (_req, res) => {
   const speaker = await talker();
-  return response.status(HTTP_OK_STATUS).json(speaker);
+  return res.status(HTTP_OK_STATUS).json(speaker);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const speaker = await talker();
+  const { id } = req.params;
+  const talkerId = speaker.find((people) => people.id === +id);
+
+  if (!talkerId) {
+    res.status(HTTP_ERROR_STATUS).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  return res.status(HTTP_OK_STATUS).json(talkerId);
 });
 
 app.listen(PORT, () => {
